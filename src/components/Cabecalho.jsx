aae99311c6bf19ac33f93eda6eabda43d8531eb5
Ivanrangelpm <rm551005@fiap.com.br>
2023-08-24 12:02:50 -0300

    Criando pagss

---
+++ src/components/Cabecalho.jsx
@@ -0,0 +1,21 @@
+export default function Cabecalho() {
+    return (
+      <>
+        <header>
+          <h1>Vite + React / Coded By - PF0670</h1>
+          <ul>
+            <li>
+              <a href="#">Item - 1</a>
+            </li>
+            <li>
+              <a href="#">Item - 2</a>
+            </li>
+            <li>
+              <a href="#">Item - 3</a>
+            </li>
+          </ul>
+        </header>
+      </>
+    );
+  }
+  
---
+++ src/components/Conteudo.jsx
@@ -0,0 +1,35 @@
+import reactLogo from "../assets/react.svg";
+
+export default function Conteudo(props) {
+
+    let altReactLogo = "Logo do React";
+
+  return (
+    <> 
+      <section>
+        <div>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores
+            quasi odio harum consectetur aperiam quisquam temporibus tempora?
+            Ducimus blanditiis dolores sit ut ratione? Veritatis, tenetur qui
+            dignissimos illo asperiores quod.
+          </p>
+          <p>
+            Obcaecati optio amet ducimus beatae. Vero quasi illum temporibus?
+            Veritatis, nemo? Autem, ea laborum consectetur nisi animi temporibus
+            beatae placeat nihil quod, delectus modi fugiat. Praesentium,
+            aspernatur numquam. Saepe, dolores!
+          </p>
+          <p>
+            Quam temporibus cumque neque. Magnam eos harum architecto libero
+            mollitia neque sit quo officia nostrum voluptates. Consectetur,
+            placeat alias dicta vitae quia, nisi quasi nam neque similique magni
+            odit obcaecati!
+          </p>
+          <img src={reactLogo} alt={altReactLogo} />
+          <img src={props.viteLogoProps} alt={props.viteLogoAltProps} />
+        </div>
+      </section>
+    </>
+  );
+}
