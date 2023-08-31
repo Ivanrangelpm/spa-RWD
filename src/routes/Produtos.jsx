25ffcb725faefaa01b9423402185fefc93b9ae03
Ivanrangelpm <rm551005@fiap.com.br>
2023-08-31 10:57:15 -0300

    tabela de produtos criada

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -1,7 +1,29 @@
 export default function Produtos() {
     return (
       <>
-      <h1>Produtos</h1>
+      <h1>Produtos informaticos - FIAPO</h1>
+
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>NOME</th>
+            <th>DESCRIÇÃO</th>
+            <th>PREÇO</th>
+
+          </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+
+        <tfoot>
+          <tr>
+            <td colSpan={3}>PRODUTOS</td>
+          </tr>
+        </tfoot>
+      </table>
+
       </>
     )
   }
