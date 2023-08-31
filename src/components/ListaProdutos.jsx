ce447b78e691e8650bf6d483dde407ece7f00379
Ivanrangelpm <rm551005@fiap.com.br>
2023-08-31 10:50:13 -0300

    Modulo ListaProdutos

---
+++ src/components/ListaProdutos.jsx
@@ -0,0 +1,7 @@
+export const ListaProdutos = [
+
+    {id:1, nome:'Teclado', desc:'Mecanico', preco: 150},
+    {id:2, nome:'Mouse', desc:'Gamer', preco: 120},
+    {id:3, nome:'Head-Set', desc:'8D', preco: 550},
+    {id:4, nome:'Monitor', desc:'32"', preco: 1500}
+];
