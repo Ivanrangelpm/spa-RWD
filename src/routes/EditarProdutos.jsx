4c474a8ca8f79191acd38ac5704aedd51199ab35
Ivanrangelpm <rm551005@fiap.com.br>
2023-08-31 12:07:37 -0300

    Arrumando editar produto

--- src/routes/EditarProdutos.jsx
+++ src/routes/EditarProdutos.jsx
@@ -1,7 +1,26 @@
+import { useParams } from "react-router-dom"
+import { ListaProdutos } from "../components/ListaProdutos";
+
 export default function EditarProdutos() {
-    return (
-      <>
+
+
+  const {id} = useParams();
+
+  const produtoRecuperado = ListaProdutos.filter((produto) => {
+     if(produto.id == id){
+        return produto;
+     }
+  });
+
+  const objProdutoRecuperado = produtoRecuperado[0];
+
+  return (
+    <>
       <h1>EditarProdutos</h1>
-      </>
-    )
-  }
+      <h2>PRODUTO</h2>
+      <p>NOME : {objProdutoRecuperado.nome}</p>
+      <p>DESCRIÇÂO : {objProdutoRecuperado.desc}</p>
+      <p>PREÇO : {objProdutoRecuperado.preco}</p>
+    </>
+  );
+}
