388dd5fc26bc64b37b0f9bf3930d5dea5bb56ba9
Ivanrangelpm <rm551005@fiap.com.br>
2023-09-13 11:44:23 -0300

    use navigate e usestate

--- src/routes/EditarProdutos.jsx
+++ src/routes/EditarProdutos.jsx
@@ -1,121 +1,100 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import { ListaProdutos } from '../components/ListaProdutos';
+import { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { ListaProdutos } from "../components/ListaProdutos";
 
 export default function EditarProdutos() {
-
   document.title = "EDITAR PRODUTO";
 
-  const {id} = useParams();
-
-  const produtoRecuperado = ListaProdutos.filter((produto) => produto.id == id)[0]
+  const navigate = useNavigate();
 
 
-      
+  const { id } = useParams();
+
+  const produtoRecuperado = ListaProdutos.filter(
+    (produto) => produto.id == id
+  )[0];
+
+  const [produto, setProduto] = useState({
+    id: produtoRecuperado.id,
+    nome: produtoRecuperado.nome,
+    desc: produtoRecuperado.desc,
+    preco: produtoRecuperado.preco,
+  });
+
+
+  const handleChange = (e) =>{
+    
+    const {name,value} = e.target;
+
+    setProduto({...produto,[name]:value});
+
+}
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+
+    let indice;
+
+    ListaProdutos.forEach((item, index) =>{
+      if(item.id == produto.id){
+        indice = index;
+      }});
+
+      ListaProdutos.splice(indice,1,produto);
+      alert("Seu produto foi alterado com sucesso!")
+      navigate("/produtos");
+  }
+
   return (
     <>
+      {/* <div>
+        <button onClick={()=> setCounter(counter + 1)}>COUNTER - {counter}</button>
+      </div> */}
+      <div>
+        <form onSubmit={handleSubmit}>
+          <fieldset>
+            <legend>Produto Selecionado</legend>
+            <div>
+              <input type="hidden" name="id" value={produto.id} />
+            </div>
+            <div>
+              <label htmlFor="idNome">Nome</label>
+              <input
+                type="text"
+                name="nome"
+                id="idNome"
+                value={produto.nome} onChange={handleChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="idDesc">Descrição</label>
+              <input
+                type="text"
+                name="desc"
+                id="idDesc"
+                defaultValue={produto.desc} onChange={handleChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="idPreco">Preço</label>
+              <input
+                type="text"
+                name="preco"
+                id="idPreco"
+                defaultValue={produto.preco} onChange={handleChange}
+              />
+            </div>
+            <div>
+              <button>EDITAR</button>
+            </div>
+          </fieldset>
+        </form>
         <div>
-          <form>
-            <fieldset>
-              <legend>Produto Selecionado</legend>
-              <div>
-                <label htmlFor="idNome">Nome</label>
-                <input type="text" name="nome" id="idNome" defaultValue={produtoRecuperado.nome}/>
-              </div>
-              <div>
-                <label htmlFor="idDesc">Descrição</label>
-                <input type="text" name="desc" id="idDesc" defaultValue={produtoRecuperado.desc}/>
-              </div>
-              <div>
-                <label htmlFor="idPreco">Preço</label>
-                <input type="text" name="preco" id="idPreco" defaultValue={produtoRecuperado.preco}/>
-              </div>
-              <div>
-                <button>EDITAR</button>
-              </div>
-            </fieldset>
-          </form>
+          <p>Nome: {produto.nome}</p>
+          <p>Desconto: {produto.desc}</p>
+          <p>Preço: {produto.preco}</p>
         </div>
+      </div>
     </>
-    
-  )
+  );
 }
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import { useParams } from "react-router-dom"
-// import { ListaProdutos } from "../components/ListaProdutos";
-
-// export default function EditarProdutos() {
-
-//   document.title = "EDITAR PRODUTO"
-
-//   const {id} = useParams();
-
-//   const produtoRecuperado = ListaProdutos.filter((produto) => {
-//      if(produto.id == id){
-//         return produto;
-//      }
-//   });
-
-//   const objProdutoRecuperado = produtoRecuperado[0];
-
-//   return (
-//     <>
-//     <div>
-//       <form>
-//         <fieldset>
-//           <legend>Produto selecionado</legend>
-//           <div>
-//           <label htmlFor="idNome">Nome</label>
-//           <input type="text" name="nome" id="idNome" defaultValue={produtoRecuperado.nome}/>
-//    </div>
-//    <div>
-//                 <label htmlFor="idDesc">Descrição</label>
-//                 <input type="text" name="desc" id="idNome" defaultValue={produtoRecuperado.nome}/>
-//               </div>
-//         </fieldset>
-//       </form>
-//     </div>
-//       <h1>EditarProdutos</h1>
-//       <h2>PRODUTO</h2>
-//       <p>NOME : {objProdutoRecuperado.nome}</p>
-//       <p>DESCRIÇÂO : {objProdutoRecuperado.desc}</p>
-//       <p>PREÇO : {objProdutoRecuperado.preco}</p>
-//     </>
-//   );
-// }
