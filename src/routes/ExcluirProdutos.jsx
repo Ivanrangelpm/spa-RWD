d3800e287af92529cf924b1d8e362a1d8255b096
Ivanrangelpm <rm551005@fiap.com.br>
2023-09-14 10:54:51 -0300

    Esqueleto de excluir produto

--- src/routes/ExcluirProdutos.jsx
+++ src/routes/ExcluirProdutos.jsx
@@ -1,16 +1,32 @@
+import { useNavigate, useParams } from "react-router-dom";
+import { ListaProdutos } from "../components/ListaProdutos";
+
 export default function ExcluirProdutos() {
-    return (
-      <>
+
+  document.title = "EXCLUIR PRODUTO";
+
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const produto = ListaProdutos.filter((produto) => produto.id == id)[0];
+
+
+
+
+  return (
+    <>
         <div>
           <h1>Excluir Produtos</h1>
+          
           <div>
+            <h2>Produto Selecionado</h2>
             <figure>
-              <img src="" alt="" />
-              <figcaption>Nome - Preço</figcaption>
+              <img src={produto.img} alt={produto.desc} />
+              <figcaption>{produto.nome} - <span>R$ {produto.preco}</span></figcaption>
             </figure>
           </div>
+
         </div>
-      </>
-    )
-  }
-  
+    </>
+    
+  )
+}
