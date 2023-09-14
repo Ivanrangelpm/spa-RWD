a04e2666c11b73febe145b956894b0fe4bf155dd
Ivanrangelpm <rm551005@fiap.com.br>
2023-09-14 12:17:27 -0300

    Excluindo produtos

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,19 @@
 import Cabecalho from "./components/Cabecalho";
-//import Conteudo from "./components/Conteudo";
 import Rodape from "./components/Rodape";
-//import viteLogo from "./assets/vite.svg";
 import { Outlet } from "react-router-dom";
+import style from "./App.module.css";
 
 export default function App() {
-  //Área de declaração de variáveis/constantes/estados/refs/contexts/funções, etc...
-
-  //let viteLogoAlt = "Logo do Vite";
-
+  //Área declarativa
+  let viteLogoAlt = "Vite Logo";
   return (
     <>
-      {/* Vamos criar uma Cabeçalho com um header, um h1 com um título indicativo sobre o projeto e uma lista ul com 3 itens com links. */}
+      {/* Área imperativa */}
+      <div className={style.container}>
       <Cabecalho />
-      <Outlet/>
-      {/* Vamos criar uma seção de conteúdo com uma div e 3 parágrafos com cada um tendo 3 linhas de lorem. Abaixo destes parágrafos vamos adicionar uma imagem */}
-
-
-      {/* Vamos criar uma seção de rodapé com uma div e uma lista ul com 3 itens com links para redes sociais. Um parágrafo com o seguinte texto: Todos os meus direitos reservado - 2023, adicione o código do símbolo de copyright.*/}
+        <Outlet/>
       <Rodape />
+      </div>
     </>
   );
 }
--- src/routes/ExcluirProdutos.jsx
+++ src/routes/ExcluirProdutos.jsx
@@ -1,5 +1,6 @@
 import { useNavigate, useParams } from "react-router-dom";
 import { ListaProdutos } from "../components/ListaProdutos";
+import style from "./ExcluirProdutos.module.css";
 
 export default function ExcluirProdutos() {
 
@@ -9,20 +10,39 @@ export default function ExcluirProdutos() {
   const { id } = useParams();
   const produto = ListaProdutos.filter((produto) => produto.id == id)[0];
 
+  const handleDelete = ()=>{
+    
+    let indice;
+    //Ou utilizando o método findIndex
+    indice = ListaProdutos.findIndex(item => item.id == produto.id);
 
+    //Alterando o produto na lista com o método splice()
+    ListaProdutos.splice(indice,1);
 
+    alert("Produto excluído com SUCESSO!");
+
+    //Redirecionando o usuáio para a página de produtos!
+    navigate('/produtos');
+  }
 
   return (
     <>
         <div>
-          <h1>Excluir Produtos</h1>
+          <h1 className={style.tit}>Excluir Produtos</h1>
           
-          <div>
+          <div className={style.card}>
             <h2>Produto Selecionado</h2>
+            <h3 className={style.alert}>Deseja realmente excluir esse produto?</h3>
             <figure>
               <img src={produto.img} alt={produto.desc} />
               <figcaption>{produto.nome} - <span>R$ {produto.preco}</span></figcaption>
             </figure>
+            
+            <div className={style.btn}>
+              <button onClick={handleDelete}>EXCLUIR</button>
+              <button onClick={()=> navigate("/produtos")}>CANCELAR</button>
+            </div>
+
           </div>
 
         </div>
