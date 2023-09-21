ef2afa37d7b8849333c270d8cd6132613bfc54ca
Ivanrangelpm <rm551005@fiap.com.br>
2023-09-21 12:29:50 -0300

    Usando dbjson

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -1,14 +1,30 @@
 import { Link } from "react-router-dom";
 import { ListaProdutos } from "../components/ListaProdutos";
-import {AiFillEdit as EditObj} from "react-icons/ai"
-import {RiDeleteBin2Fill as DelObj} from "react-icons/ri";
+import { AiFillEdit as EditObj } from "react-icons/ai";
+import { RiDeleteBin2Fill as DelObj } from "react-icons/ri";
 import estilos from "./Produtos.module.css";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export default function Produtos() {
-
   document.title = "Lista de Produtos";
 
-  
+  const [listaProdutosLocal, setListaProdutosLocal] = useState([{}]);
+
+  useEffect(() => {
+    //Criando o bloco de reequisição dos dados utilizando o fetch com promises:
+fetch("http://localhost:5000/produtos", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setListaProdutosLocal(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <>
@@ -25,16 +41,27 @@ export default function Produtos() {
             <th>EDITAR/EXCLUIR</th>
           </tr>
         </thead>
-        
-        <tbody> 
-          {ListaProdutos.map((produto, indice) => (
+
+        <tbody>
+          {listaProdutosLocal.map((produto, indice) => (
             <tr key={indice} className={estilos.tblLine}>
               <td>{produto.id}</td>
               <td>{produto.nome}</td>
               <td>{produto.desc}</td>
               <td>{produto.preco}</td>
-              <td><img src={produto.img} alt={produto.desc} /></td>
-              <td> <Link to={`/editar/produtos/${produto.id}`}><EditObj/></Link> | <Link to={`/excluir/produtos/${produto.id}`}><DelObj/></Link></td>
+              <td>
+                <img src={produto.img} alt={produto.desc} />
+              </td>
+              <td>
+                {" "}
+                <Link to={`/editar/produtos/${produto.id}`}>
+                  <EditObj />
+                </Link>{" "}
+                |{" "}
+                <Link to={`/excluir/produtos/${produto.id}`}>
+                  <DelObj />
+                </Link>
+              </td>
             </tr>
           ))}
         </tbody>
