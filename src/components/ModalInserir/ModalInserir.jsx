44c39cf37131e58297a59640d7d8b6ff5d6b4466
Ivanrangelpm <rm551005@fiap.com.br>
2023-09-27 12:16:37 -0300

    Insert no modal

--- src/components/ModalInserir/ModalInserir.jsx
+++ src/components/ModalInserir/ModalInserir.jsx
@@ -2,7 +2,26 @@ import styles from "./ModalInserir.module.css"
 
 
 export default function ModalInserir(props) {
- 
+
+   document.title= "CADASTRO";
+   
+   let novoId;
+
+   fetch("http://localhost:5000/produtos",{
+    method: "GET",
+    headers:{
+        "Content-Type":"application/json"
+    }
+   })
+   .then((response)=> response.json())
+   .then((data)=>{
+    console.log(data)
+    novoId = (data[data.length-1].id+1)
+    console.log("NOVO ID " + novoId);
+    return novoId;
+    
+    })
+.catch(error=> console.log(error));
     if(props.open){
         return (
         <div className={styles.container}>
