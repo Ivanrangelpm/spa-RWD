0c4b6f957593a49f3edc2e1ca564c179d8c5ad64
Ivanrangelpm <rm551005@fiap.com.br>
2023-09-28 10:43:14 -0300

    Retirando arquivos sem utilização do boylerplate e adc img

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,8 +1,13 @@
 
 export default function Home() {
   return (
-    <>
+    <div>
     <h1>Home</h1>
-    </>
+    <figure>
+      <figcaption>NOSSOS PRODUTOS</figcaption>
+      <img src="/img/produtos_home.jpg" alt="produtos" />
+      
+    </figure>
+    </div>
   )
 }
