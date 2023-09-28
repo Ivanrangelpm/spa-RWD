48c392b42b32c429101e2e5b1f9103453191e139
Ivanrangelpm <rm551005@fiap.com.br>
2023-09-28 11:40:20 -0300

    Estilo SASS inserido e Form Modal criado.

--- src/components/ModalInserir/ModalInserir.jsx
+++ src/components/ModalInserir/ModalInserir.jsx
@@ -1,5 +1,5 @@
 import styles from "./ModalInserir.module.css"
-
+import "./ModalInserir.scss"
 
 export default function ModalInserir(props) {
 
@@ -17,16 +17,44 @@ export default function ModalInserir(props) {
    .then((data)=>{
     console.log(data)
     novoId = (data[data.length-1].id+1)
-    console.log("NOVO ID " + novoId);
     return novoId;
     
     })
 .catch(error=> console.log(error));
+
+
+
     if(props.open){
         return (
         <div className={styles.container}>
-            <h1>Modal Inserir</h1>
+            <h1>CADASTRO DE PRODUTOS</h1>
             <button onClick={() => props.setOpen(false)}>CLOSE-MODAL</button>
+            <div>
+                <form>
+                    <fieldset>
+                        <legend>Novo Produto</legend>
+                        <div>
+                            <label htmlFor="idNome">Nome</label>
+                            <input type="text" name="nome" id="idNome" placeholder="Digite o nome do produto"/>
+                        </div>
+                        <div>
+                            <label htmlFor="idDesc">Descrição</label>
+                            <input type="text" name="desc" id="idDesc" placeholder="Digite a descrição do produto"/>
+                        </div>
+                        <div>
+                            <label htmlFor="idPreco">Preço</label>
+                            <input type="number" name="preco" id="idPreco" placeholder="Digite o valor do produto"/>
+                        </div>             
+                        <div>
+                            <label htmlFor="idImg">Imagem</label>
+                            <input type="url" name="img" id="idImg" placeholder="Digite a url da imagem do produto"/>
+                        </div>   
+                        <div>
+                            <button>CADASTRAR</button>    
+                        </div>          
+                    </fieldset>
+                </form>
+            </div>
         </div>
         );
     }
--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -36,7 +36,7 @@ fetch("http://localhost:5000/produtos", {
       <h1>Produtos Informáticos - FIAPO</h1>
       { open ? <ModalInserir open={open} setOpen={setOpen}/> : "" }
 
-      <button onClick={()=> setOpen(true)}>OPEN-MODAL</button>
+      <Link onClick={()=> setOpen(true)}>Cadastrar Produtos</Link>
 
       <table className={estilos.tblEstilo}>
         <thead>
