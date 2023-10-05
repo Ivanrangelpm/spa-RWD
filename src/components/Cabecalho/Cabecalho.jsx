4c9f51b99ae19c05a14f8bb52c569a6329dffd34
Ivanrangelpm <rm551005@fiap.com.br>
2023-10-05 11:41:11 -0300

    Adicionando marcação no link escolhido

---
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -0,0 +1,20 @@
+import { Link, useLocation } from "react-router-dom";
+import "./Cabecalho.scss";
+
+export default function Cabecalho(){
+
+    const rotaAtual = useLocation();
+   
+    return(
+        <>
+            <header className="cabecalho">
+                <nav>
+                    <ul>
+                        <li><Link to="/" className={ rotaAtual.pathname == "/" ? "active" : ""}>Home</Link></li>
+                        <li><Link to="/produtos" className={ rotaAtual.pathname == "/produtos" ? "active" : ""}>Produtos</Link></li>
+                    </ul>
+                </nav>
+            </header>
+        </>
+    )
+}
